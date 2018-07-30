28551b90a30eec23596286bf13126272d8be6388
N Eliseo S Carranza <neli92santscz@gmail.com>
2018-07-30 18:19:24 -0400

    a

--- webpack.config.js
+++ webpack.config.js
@@ -23,11 +23,11 @@ module.exports={
     },
     devServer:{
             port:3000,
-            open:true,
+            hot:true,
             contentBase:path.resolve(__dirname, "./dist"),
             proxy:{
                 "/api":"http://localhost:8080"
-            }
+            },
     },
     module:{
         rules:[
